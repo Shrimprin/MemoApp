55e7af64546bd05e66ee4e412975b0972902dcf5
Kuroneko <ken.aster8@gmail.com>
2023-09-12 18:06:41 +0900

    Memoクラスのオブジェクトを生成して使うように修正

--- memo.rb
+++ memo.rb
@@ -9,83 +9,85 @@ class Memo
   MEMOS_TABLE = 'memos'
   private_constant :MEMOS_TABLE
 
-  class << self
-    def create_table
-      result = @conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos'")
-      @conn.exec("CREATE TABLE #{MEMOS_TABLE} (id uuid default gen_random_uuid() primary key, title text not null, content text)") if result.values.empty?
-    end
+  private
 
-    def create_db
-      config = YAML.load_file('config.yml')
-      conn = PG.connect(
+  def initialize
+    create_db
+    connect_to_db
+    create_table
+  end
+
+  def create_table
+    result = @conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos'")
+    @conn.exec("CREATE TABLE #{MEMOS_TABLE} (id uuid default gen_random_uuid() primary key, title text not null, content text)") if result.values.empty?
+  end
+
+  def create_db
+    config = YAML.load_file('config.yml')
+    conn = PG.connect(
+      host: config['host'],
+      dbname: 'postgres',
+      user: config['user'],
+      password: config['password'],
+      port: config['port']
+    )
+    result = conn.exec("SELECT 1 FROM pg_database WHERE datname = '#{config['db']}'")
+    conn.exec("CREATE DATABASE #{config['db']} ENCODING 'UTF-8' TEMPLATE template0") if result.values.empty?
+  rescue PG::Error => e
+    warn "データベースの作成に失敗しました。: #{e.message}"
+  ensure
+    conn&.close
+  end
+
+  def connect_to_db
+    config = YAML.load_file('config.yml')
+    begin
+      @conn = PG.connect(
         host: config['host'],
-        dbname: 'postgres',
+        dbname: config['db'],
         user: config['user'],
         password: config['password'],
         port: config['port']
       )
-      result = conn.exec("SELECT 1 FROM pg_database WHERE datname = '#{config['db']}'")
-      conn.exec("CREATE DATABASE #{config['db']} ENCODING 'UTF-8' TEMPLATE template0") if result.values.empty?
-    rescue PG::Error => e
-      warn "データベースの作成に失敗しました。: #{e.message}"
-    ensure
-      conn&.close
+    rescue PG::ConnectionBad => e
+      warn "DBに接続できません。config.ymlを確認してください。\n#{e}"
+      exit
     end
+  end
 
-    def connect_to_db
-      config = YAML.load_file('config.yml')
-      begin
-        @conn = PG.connect(
-          host: config['host'],
-          dbname: config['db'],
-          user: config['user'],
-          password: config['password'],
-          port: config['port']
-        )
-      rescue PG::ConnectionBad => e
-        warn "DBに接続できません。config.ymlを確認してください。\n#{e}"
-        exit
-      end
-    end
+  public
 
-    def setup
-      create_db
-      connect_to_db
-      create_table
-    end
+  def find_memos
+    memo_list = @conn.exec("SELECT * FROM #{MEMOS_TABLE}")
+    memo_list.map { |memo| { 'id' => memo['id'], 'title' => memo['title'] } }
+  end
 
-    def find_memos
-      memo_list = @conn.exec("SELECT * FROM #{MEMOS_TABLE}")
-      memo_list.map { |memo| { 'id' => memo['id'], 'title' => memo['title'] } }
-    end
+  def add_new_memo(memo_title, memo_content)
+    insert_query = "INSERT INTO #{MEMOS_TABLE} (title, content) VALUES($1, $2)"
+    insert_params = [memo_title, memo_content]
+    @conn.exec(insert_query, insert_params)
+  end
 
-    def add_new_memo(memo_title, memo_content)
-      insert_query = "INSERT INTO #{MEMOS_TABLE} (title, content) VALUES($1, $2)"
-      insert_params = [memo_title, memo_content]
-      @conn.exec(insert_query, insert_params)
-    end
+  def find_memo(id)
+    select_query = "SELECT * FROM #{MEMOS_TABLE} WHERE id = $1"
+    select_params = [id]
+    memo = @conn.exec(select_query, select_params).tuple_values(0)
+    {
+      id: memo[0],
+      title: memo[1],
+      content: memo[2]
+    }
+  end
 
-    def find_memo(id)
-      select_query = "SELECT * FROM #{MEMOS_TABLE} WHERE id = $1"
-      select_params = [id]
-      memo = @conn.exec(select_query, select_params).tuple_values(0)
-      {
-        id: memo[0],
-        title: memo[1],
-        content: memo[2]
-      }
-    end
+  def update_memo(id, memo_title, memo_content)
+    update_query = "UPDATE #{MEMOS_TABLE} SET title = $1, content = $2 WHERE id = $3"
+    update_params = [memo_title, memo_content, id]
+    @conn.exec(update_query, update_params)
+  end
 
-    def update_memo(id, memo_title, memo_content)
-      update_query = "UPDATE #{MEMOS_TABLE} SET title = $1, content = $2 WHERE id = $3"
-      update_params = [memo_title, memo_content, id]
-      @conn.exec(update_query, update_params)
-    end
-
-    def delete_memo(id)
-      delete_query = "DELETE FROM #{MEMOS_TABLE} WHERE id = $1"
-      delete_params = [id]
-      @conn.exec(delete_query, delete_params)
-    end
+  def delete_memo(id)
+    delete_query = "DELETE FROM #{MEMOS_TABLE} WHERE id = $1"
+    delete_params = [id]
+    @conn.exec(delete_query, delete_params)
   end
 end
--- router.rb
+++ router.rb
@@ -1,75 +1,81 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'sinatra'
+require 'sinatra/base'
 require './memo'
 
-configure do
-  Memo.setup
+class Router < Sinatra::Base
+  enable :method_override
+
+  before do
+    @memo = Memo.new
+  end
+
+  get '/' do
+    redirect '/memos'
+  end
+
+  get '/memos' do
+    # メモ一覧(ホーム画面)の表示
+    @memos_id_title = @memo.find_memos
+    @page_title = 'メモ一覧'
+
+    erb :memos
+  end
+
+  get '/memos/new' do
+    # 新規メモ作成画面の表示
+    @page_title = '新規作成'
+
+    erb :new
+  end
+
+  post '/memos' do
+    # 新規メモの保存
+    @memo.add_new_memo(params[:memo_title], params[:memo_content])
+
+    redirect '/memos'
+  end
+
+  get '/memos/:id' do |id|
+    # メモの表示
+    @memo_data = @memo.find_memo(id)
+    halt 404 if @memo_data.nil?
+
+    erb :show
+  end
+
+  get '/memos/:id/edit' do |id|
+    # メモの編集画面の表示
+    @memo_data = @memo.find_memo(id)
+    halt 404 if @memo_data.nil?
+
+    erb :edit
+  end
+
+  patch '/memos/:id' do |id|
+    # メモの更新
+    @memo.update_memo(id, params[:memo_title], params[:memo_content])
+
+    redirect '/memos'
+  end
+
+  delete '/memos/:id' do |id|
+    # メモの削除
+    @memo.delete_memo(id)
+
+    redirect '/memos'
+  end
+
+  error do
+    status 500
+    '500 Server Error'
+  end
+
+  not_found do
+    status 404
+    '404 Not Found - ページが存在しません'
+  end
 end
 
-get '/' do
-  redirect '/memos'
-end
-
-get '/memos' do
-  # メモ一覧(ホーム画面)の表示
-  @memos_id_title = Memo.find_memos
-  @page_title = 'メモ一覧'
-
-  erb :memos
-end
-
-get '/memos/new' do
-  # 新規メモ作成画面の表示
-  @page_title = '新規作成'
-
-  erb :new
-end
-
-post '/memos' do
-  # 新規メモの保存
-  Memo.add_new_memo(params[:memo_title], params[:memo_content])
-
-  redirect '/memos'
-end
-
-get '/memos/:id' do |id|
-  # メモの表示
-  @memo_data = Memo.find_memo(id)
-  halt 404 if @memo_data.nil?
-
-  erb :show
-end
-
-get '/memos/:id/edit' do |id|
-  # メモの編集画面の表示
-  @memo_data = Memo.find_memo(id)
-  halt 404 if @memo_data.nil?
-
-  erb :edit
-end
-
-patch '/memos/:id' do |id|
-  # メモの更新
-  Memo.update_memo(id, params[:memo_title], params[:memo_content])
-
-  redirect '/memos'
-end
-
-delete '/memos/:id' do |id|
-  # メモの削除
-  Memo.delete_memo(id)
-
-  redirect '/memos'
-end
-
-error do
-  status 500
-  '500 Server Error'
-end
-
-not_found do
-  status 404
-  '404 Not Found - ページが存在しません'
-end
+Router.run!
